d095f86be352a52f75cc509aca32731535b929a5
Egbewatt Kokou Mwangah Smith <ksupro1@gmail.com>
2024-02-05 08:39:53 -0700

    test cleanup

--- test/cluster/scripts/master.sh
+++ test/cluster/scripts/master.sh
@@ -6,17 +6,10 @@ set -euxo pipefail
 
 MASTER_IP="10.0.0.10"
 NODENAME=$(hostname -s)
-POD_CIDR="192.168.0.0/16"
 
-sudo kubeadm config images pull
-
-echo "Preflight Check Passed: Downloaded All Required Images"
-
-sudo kubeadm init --apiserver-advertise-address=$MASTER_IP --apiserver-cert-extra-sans=$MASTER_IP --pod-network-cidr=$POD_CIDR --node-name=$NODENAME --ignore-preflight-errors Swap
-
-mkdir -p "$HOME"/.kube
-sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
-sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
+mkdir -p "$HOME"/.taskweaver
+sudo cp -i /etc/taskweaver/admin.conf "$HOME"/.taskweaver/config
+sudo chown "$(id -u)":"$(id -g)" "$HOME"/.taskweaver/config
 
 # Save Configs to shared /Vagrant location
 
@@ -30,24 +23,6 @@ else
   mkdir -p $config_path
 fi
 
-cp -i /etc/kubernetes/admin.conf /vagrant/configs/config
+cp -i /etc/taskweaver/admin.conf /vagrant/configs/config
 touch /vagrant/configs/join.sh
 chmod +x /vagrant/configs/join.sh
-
-kubeadm token create --print-join-command > /vagrant/configs/join.sh
-
-# Install Calico Network Plugin
-# curl https://docs.projectcalico.org/manifests/calico.yaml -O
-# kubectl apply -f calico.yaml
-
-kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
-kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
-kubectl get pods -n calico-system
-
-# Install Metrics Server
-kubectl apply -f https://raw.githubusercontent.com/scriptcamp/kubeadm-scripts/main/manifests/metrics-server.yaml
-
-# Install Kubernetes Dashboard
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.1/aio/deploy/recommended.yaml
-
