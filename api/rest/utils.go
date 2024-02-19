0daf4d758c25e9b8b9fa0b6c3f6a3530402916e7
Egbewatt Kokou Mwangah Smith <ksupro1@gmail.com>
2024-02-18 23:18:19 -0700

    handle request json

--- api/rest/utils.go
+++ api/rest/utils.go
@@ -1,9 +1,11 @@
 package rest
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"regexp"
 	"strings"
 
@@ -85,4 +87,21 @@ func PrintMuxRoutes(logger *log.Logger, router *mux.Router) {
 		}
 		return err
 	})
+}
+
+func encode[T any](w http.ResponseWriter, r *http.Request, status int, v T) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		return fmt.Errorf("encode json: %w", err)
+	}
+	return nil
+}
+
+func decode[T any](r *http.Request) (T, error) {
+	var v T
+	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+		return v, fmt.Errorf("decode json: %w", err)
+	}
+	return v, nil
 }
